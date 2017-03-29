44337ab893b42c794a1b9089fdbb777660f183f5
Cory Romdenne <cory@romdenne.com>
2017-03-29 15:03:21 -0500

    Sunset Sunrise API
    Implemented API call to sunrise-sunset.org. Formatted responses to
    display sunrise, sunset and daylength. Calling API based on current
    date, must update to take custom date.

--- js/main.js
+++ js/main.js
@@ -2,13 +2,52 @@ var autocomplete = new google.maps.places.Autocomplete((document.getElementById(
 	types: ['(cities)']
 });
 
+toHHMMSS = function (sec_num) {
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
+}
+
+APIcallback = function(data) {
+	var status = data.status;
+	console.log("sunrise-sunset.org API call status: " + status);
+
+	if (status == "OK") {
+		var sunrise = new Date(data.results.sunrise);
+		var sunset = new Date(data.results.sunset);
+		var daylength = toHHMMSS(data.results.day_length);
+
+		$("#sunrise-time").text(sunrise.toLocaleTimeString());
+		$("#daylength-time").text(daylength);
+		$("#sunset-time").text(sunset.toLocaleTimeString());
+	}
+	$("#content-generated").toggle();
+	$("#footer").toggle();
+};
+
 $("#search-submit").click(function(){
 	if($("#search-location").val() != "") {
 		var place = autocomplete.getPlace();
 		var address = place.formatted_address;
 		var coordinates = place.geometry.location;
+		var date = new Date();
 		$("#title-location").text(address);
-		$("#title-coordinates").text(coordinates);
+		$("#title-date").text(date.toLocaleDateString());
+
+		var latitude = coordinates.lat();
+		var longitude = coordinates.lng();
+		var query = "http://api.sunrise-sunset.org/json?lat=" + latitude + "&lng=" + longitude + "&date=today&formatted=0&callback=APIcallback";
+
+		$.ajax({
+			url: query,
+			dataType: "jsonp",
+			jsonpCallback: "APIcallback"
+		});
 	} else {
 		return false;
 	}
