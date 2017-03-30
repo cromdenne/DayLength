29337c4afae3027ed2f1992cd803969417d8e48b
Cory Romdenne <cory@romdenne.com>
2017-03-30 11:27:52 -0500

    Trend data and styling
    Trend data now populates based on date input. Styled entire trend data
    area as one table rather than separate tables.

--- js/main.js
+++ js/main.js
@@ -22,27 +22,94 @@ toHHMMSS = function (sec_num) {
     return hours+':'+minutes+':'+seconds;
 }
 
-// callback from sunrise-sunset API
-APIcallback = function(data) {
+// convert day of week to string
+toDay = function (day_num) {
+	if (day_num == 0) { return "Sunday"; }
+	else if (day_num == 1) { return "Monday" }
+	else if (day_num == 2) { return "Tuesday" }
+	else if (day_num == 3) { return "Wednesday" }
+	else if (day_num == 4) { return "Thursday" }
+	else if (day_num == 5) { return "Friday" }
+	else if (day_num == 6) { return "Saturday" }
+	else {
+		console.log(day_num + " is not a valid day.");
+		return;
+	}
+}
+
+// remove space between time and AM/PM notation
+noSpace = function (time) {
+	var timeparts = time.split(" ");
+	return timeparts[0] + timeparts[1];
+}
+
+// callback from sunrise-sunset API for main data
+APIcallbackMain = function(data) {
 	var status = data.status;
-	console.log("sunrise-sunset.org API call status: " + status);
+	console.log("sunrise-sunset.org API call status main: " + status);
 
 	if (status == "OK") {
 		var sunrise = new Date(data.results.sunrise);
 		var sunset = new Date(data.results.sunset);
 		var daylength = toHHMMSS(data.results.day_length);
 
-		$("#sunrise-time").text(sunrise.toLocaleTimeString());
+		// set main data
+		$("#sunrise-time").text(noSpace(sunrise.toLocaleTimeString()));
 		$("#daylength-time").text(daylength);
-		$("#sunset-time").text(sunset.toLocaleTimeString());
+		$("#sunset-time").text(noSpace(sunset.toLocaleTimeString()));
+
+		// set trend data
+		$("#forecast-current-date").text(sunrise.toLocaleDateString());
+		$("#forecast-current-day").text(toDay(sunrise.getDay()));
+		$("#forecast-current-sunrise").text(noSpace(sunrise.toLocaleTimeString()));
+		$("#forecast-current-daylength").text(daylength);
+		$("#forecast-current-sunset").text(noSpace(sunset.toLocaleTimeString()));
 	}
 	$("#content-generated").toggle();
 	$("#footer").toggle();
 };
 
+// callback from sunrise-sunset API for trend data past
+APIcallbackTrendPast = function(data) {
+	var status = data.status;
+	console.log("sunrise-sunset.org API call status trend past: " + status);
+
+	if (status == "OK") {
+		var sunrise = new Date(data.results.sunrise);
+		var sunset = new Date(data.results.sunset);
+		var daylength = toHHMMSS(data.results.day_length);
+
+		$("#forecast-before-date").text(sunrise.toLocaleDateString());
+		$("#forecast-before-day").text(toDay(sunrise.getDay()));
+		$("#forecast-before-sunrise").text(noSpace(sunrise.toLocaleTimeString()));
+		$("#forecast-before-daylength").text(daylength);
+		$("#forecast-before-sunset").text(noSpace(sunset.toLocaleTimeString()));
+	}
+}
+
+// callback from sunrise-sunset API for trend data future
+APIcallbackTrendFuture = function(data) {
+	var status = data.status;
+	console.log("sunrise-sunset.org API call status trend future: " + status);
+
+	if (status == "OK") {
+		var sunrise = new Date(data.results.sunrise);
+		var sunset = new Date(data.results.sunset);
+		var daylength = toHHMMSS(data.results.day_length);
+
+		$("#forecast-after-date").text(sunrise.toLocaleDateString());
+		$("#forecast-after-day").text(toDay(sunrise.getDay()));
+		$("#forecast-after-sunrise").text(noSpace(sunrise.toLocaleTimeString()));
+		$("#forecast-after-daylength").text(daylength);
+		$("#forecast-after-sunset").text(noSpace(sunset.toLocaleTimeString()));
+	}
+}
+
 // handle submission of location and date
 $("#search-submit").click(function(){
 	if($("#search-location").val() != "") {
+		
+		// set important data
 		var place = autocomplete.getPlace();
 		var address = place.formatted_address;
 		var coordinates = place.geometry.location;
@@ -50,6 +117,7 @@ $("#search-submit").click(function(){
 
 		$("#title-location").text(address);
 
+		// date could be left blank, if so then default to current date
 		if($("#search-date").val() != "") {
 			date = new Date($("#search-date").val());
 		} else {
@@ -57,19 +125,43 @@ $("#search-submit").click(function(){
 		}
 		$("#title-date").text(date.toLocaleDateString());
 
+		// prep API URL
 		var latitude = coordinates.lat();
 		var longitude = coordinates.lng();
 		var query = "http://api.sunrise-sunset.org/json?lat=" + latitude 
 					+ "&lng=" + longitude 
 					+ "&date=" + date.toLocaleDateString() 
-					+ "&formatted=0&callback=APIcallback";
+					+ "&formatted=0&callback=APIcallbackMain";
 	
-
+		// call API for main data
 		$.ajax({
 			url: query,
 			dataType: "jsonp",
-			jsonpCallback: "APIcallback"
+			jsonpCallback: "APIcallbackMain"
 		});
+
+		// prep and call API URL for trend data past
+		var trendPast = "http://api.sunrise-sunset.org/json?lat=" + latitude 
+					+ "&lng=" + longitude 
+					+ "&date=-7 day " + date.toLocaleDateString() 
+					+ "&formatted=0&callback=APIcallbackTrendPast";
+		$.ajax({
+			url: trendPast,
+			dataType: "jsonp",
+			jsonpCallback: "APIcallbackTrendPast"
+		});
+
+		// prep and call API URL for trend data future
+		var trendFuture = "http://api.sunrise-sunset.org/json?lat=" + latitude 
+					+ "&lng=" + longitude 
+					+ "&date=+7 day " + date.toLocaleDateString() 
+					+ "&formatted=0&callback=APIcallbackTrendFuture";
+		$.ajax({
+			url: trendFuture,
+			dataType: "jsonp",
+			jsonpCallback: "APIcallbackTrendFuture"
+		});
+
 	} else {
 		return false;
 	}
